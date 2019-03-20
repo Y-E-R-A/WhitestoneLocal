07f5804e04bb8fed8764401c206805b71d15076e
Yomaira Rivera <yomaira.rivera1@upr.edu>
2019-03-20 17:47:00 -0400

    Main

--- main.py
+++ main.py
@@ -13,30 +13,52 @@
 # Senate of the UPRM                        #
 #############################################
 
-from flask_cors import CORS, cross_origin
-from flask import Flask, request
-from handler.ActivityLogHandler import ActivityLogHandler
-from handler.AudioHandler import AudioHandler
-from handler.ChooseHandler import ChooseHandler
-from handler.CredentialHandler import CredentialHandler
-from handler.MeetingHandler import MeetingHandler
-from handler.ParticipatesInHandler import ParticipatesInHandler
-from handler.UsersHandler import UsersHandler
-from handler.VoteInHandler import VoteInHandler
-from handler.VotingChoiceHandler import VotingChoiceHandler
-from handler.VotingQuestionHandler import VotingQuestionHandler
 
+
+from flask import Flask, request
+
+from handler.CredentialHandler import CredentialHandler
+from handler.usersHandler import UsersHandler
 
 app = Flask(__name__)
 
-# Apply CORS to this app
-
-CORS(app)
-
-@app.route('/')
-
+@app.route('/Whitestone')
 def home():
 
     return "Welcome to Whitestone"
 
 
+##SEARCH ALL USERS
+
+@app.route('/Whitestone/users', methods = ['GET','POST'])
+
+def getAllUsers():
+
+    if request.method == 'POST':
+
+        print ("REQUEST", request.json)
+        return UsersHandler().insertCredentialsJSON(request.json)
+    else:
+
+        if request.args:
+            return UsersHandler.getAllUsersNames(request.args)
+
+        else:
+
+            handler = UsersHandler()
+            return handler.getAllUsersNames()
+
+
+
+@app.route('/Whitestone/credentials/<int:cid>')
+
+def getCredentialsById(cid):
+
+    return CredentialHandler().getCredentialById(cid)
+
+
+
+
+if __name__ == '__main__':
+
+    app.run()
