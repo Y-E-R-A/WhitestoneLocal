ce87441a85b9fed47a428070ae9709be53ef8c9d
Gustavohernandez1 <gustavo.hernandez2@upr.edu>
2019-03-30 17:31:00 -0400

    Fixed transition functions

--- Whitestone_app/appjs/activityLog.js
+++ Whitestone_app/appjs/activityLog.js
@@ -1,53 +1,54 @@
-angular.module('Whitestone').controller('activityLogController', ['$http', '$log', '$scope','$location', '$routeParams', 
-    function($http, $log, $scope, $location, $routeParams) {
-        
+angular.module('Whitestone').controller('activityLogController', ['$http', '$log', '$scope', '$location', '$routeParams',
+    function ($http, $log, $scope, $location, $routeParams) {
+
         var thisCtrl = this;
-        $scope.nowdate = Date();
+        //$scope.nowdate = Date();
 
         var date = "";
 
         this.newActivityLogList = [];
 
 
-        this.searchActivity = function(){
+        this.searchActivity = function () {
 
             // First set up the url for te route
             var data = [];
             data.date = thisCtrl.date;
-            
+
 
             var reqURL = "http://localhost:5000/whitestone/activitylog";
             // Now set up the $http object
             // It has two function call backs, one for success and one for error
-            var config = { headers : 
-                {'Content-Type':'application/json;charset=utf-8;' }
-               }
+            var config = {
+                headers:
+                    { 'Content-Type': 'application/json;charset=utf-8;' }
+            }
 
-            $http.get(reqURL,data,config).then(// success call back
-                function (response){
-                // The is the sucess function!
-                // Copy the list of parts in the data variable
+            $http.get(reqURL).then(// success call back
+                function (response) {
+                    // The is the sucess function!
+                    // Copy the list of parts in the data variable
                     console.log("response: " + JSON.stringify(response))
-                    
-                    thisCtrl.newActivityLogList = response.data;
-                    
+
+                    //thisCtrl.newActivityLogList = response.data;
+                    //thisCtrl.newActivityLogList = response.data;
                 }, // error callback
-                function (response){
+                function (response) {
                     // This is the error function
                     // If we get here, some error occurred.
                     // Verify which was the cause and show an alert.
                     console.log("response2: " + JSON.stringify(response));
                     var status = response.status;
-                    if (status == 0){
+                    if (status == 0) {
                         alert("No hay conexion a Internet");
                     }
-                    else if (status == 401){
+                    else if (status == 401) {
                         alert("Su sesion expiro. Conectese de nuevo.");
                     }
-                    else if (status == 403){
+                    else if (status == 403) {
                         alert("No esta autorizado a usar el sistema.");
                     }
-                    else if (status == 404){
+                    else if (status == 404) {
                         alert("No se encontro la informacion solicitada.");
                     }
                     else {
@@ -56,10 +57,11 @@ angular.module('Whitestone').controller('activityLogController', ['$http', '$log
                 }
             );
         };
-        this.meetingRedirect = function(){
-                                    $location.url("/meeting/"+role+'/'+uid);
-        }this.voteRedirect = function(){
-            $location.url('/Vote/'+$routeParams.role+'/'+$routeParams.uid);
+        this.createUserRedirect = function () {
+            $location.url("/createUser/" + $routeParams.role + '/' + $routeParams.uid);
         }
-        
-}]);
+
+        this.editUserRedirect = function () {
+            $location.url("/editUser/" + $routeParams.role + '/' + $routeParams.uid);
+        }
+    }]);
