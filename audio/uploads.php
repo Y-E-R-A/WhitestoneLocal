6fa7626b549e4271b954d51350b3a50a8de692da
Ariel Torres <ariel.torres1@upr.edu>
2019-04-02 21:28:19 -0400

    Changes to audio uploads

--- audio/uploads.php
+++ audio/uploads.php
@@ -3,11 +3,11 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_FILES['files'])) {
         $errors = [];
-        $path = 'uploads/';
+        $path = '/var/www/html/uploads/';
         $extensions = ['mp3'];
 
         $all_files = count($_FILES['files']['tmp_name']);
-
+        print("Number of files" . $all_files);
         for ($i = 0; $i < $all_files; $i++) {
             $file_name = $_FILES['files']['name'][$i];
             $file_tmp = $_FILES['files']['tmp_name'][$i];
@@ -16,7 +16,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $file_ext = strtolower(end(explode('.', $_FILES['files']['name'][$i])));
 
             $file = $path . $file_name;
-
+            print_r("File name: " . $file_name);
+            print_r("File temporary name: " . $file_tmp);
+            print_r("File type: " . $file_type);
+            print_r("File size: " . $file_size);
+            print_r("Complete file identifier: " . $file);
             if (!in_array($file_ext, $extensions)) {
                 $errors[] = 'Extension not allowed: ' . $file_name . ' ' . $file_type;
             }
@@ -31,5 +35,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
 
         if ($errors) print_r($errors);
+
+
+
     }
 }
