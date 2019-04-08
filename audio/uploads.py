7f267b766c1fcf4f38e670a875772d291bc91f56
Ariel Torres <ariel.torres1@upr.edu>
2019-04-08 14:52:20 -0400

    Created alternative uploads script

---
+++ audio/uploads.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3.5 -w
+print ("Content-type: text/html\r\n\r\n")
+
+import os
+from flask import Flask, request, redirect, url_for
+#from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/uploads/'
+ALLOWED_EXTENSIONS = set(['mp3'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+print("Uploads script is executing.")
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return ""
