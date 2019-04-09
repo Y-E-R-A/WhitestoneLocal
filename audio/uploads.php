9cb53e90782cdc990a3181f8830908cb81317f8e
Ariel Torres <ariel.torres1@upr.edu>
2019-04-09 17:55:31 -0400

    Updated script to a functional state

--- audio/uploads.php
+++ audio/uploads.php
@@ -1,22 +1,30 @@
 <?php
-
-echo("Here.\n");
+// echo(phpinfo());
+echo("\nFolder upload_tmp_dir is: " . ini_get('upload_tmp_dir'));
+// echo("\nFolder sys_get_tmp_dir is: " . sys_get_tmp_dir());
+echo("\nHere.\n");
 if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
     echo("REQUEST_METHOD === POST\n");
-   if (isset($_FILES['files'])) {
+   if (isset($_FILES['file'])) {
         echo("FILES is set.\n");
         $errors = [];
-        $path = '/var/www/html/uploads/';
+        $path = 'uploads/';
        // $extensions = ['mp3'];
-
+       $file_tmp = $_FILES['file']['tmp_name'];
+       $file_name = $_FILES['file']['name'];
+       $file = $path . $file_name;
+       move_uploaded_file($file_tmp, $file);
+       echo("File uploaded: ". $file);
+       var_dump($_FILES);
         //$all_files = count($_FILES['files']['tmp_name']);
     //    echo("Number of files: " . $all_files);
         for ($i = 0; $i < $all_files; $i++) {
-            $file_name = $_FILES['files']['name'][$i];
-            $file_tmp = $_FILES['files']['tmp_name'][$i];
-        //    $file_type = $_FILES['files']['type'][$i];
-            $file_size = $_FILES['files']['size'][$i];
-            $file_ext = strtolower(end(explode('.', $_FILES['files']['name'][$i])));
+            echo("Inside if statement.");
+            $file_name = $_FILES['file']['name'][$i];
+            $file_tmp = $_FILES['file']['tmp_name'][$i];
+        //    $file_type = $_FILES['file']['type'][$i];
+            $file_size = $_FILES['file']['size'][$i];
+            $file_ext = strtolower(end(explode('.', $_FILES['file']['name'][$i])));
 
             $file = $path . $file_name;
             echo(print_r("File name: " . $file_name));
@@ -32,12 +40,13 @@ if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') 
            //     $errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
            // }
 
-            if (empty($errors)) {
+           // if (empty($errors)) {
                 move_uploaded_file($file_tmp, $file);
-            }
+                echo("File uploaded.");
+           // }
         }
 
-        if ($errors) echo(print_r($errors));
+        //if ($errors) echo(print_r($errors));
 
 
 
