8a670b57fc3cb336f97a3ce6fd94f6bfef066f92
Ariel Torres <ariel.torres1@upr.edu>
2019-04-11 02:48:30 -0400

    Revamped audio files
    The audio recorder's interface has been changed and the files have been documented.

--- audio/uploads.php
+++ audio/uploads.php
@@ -1,54 +1,30 @@
 <?php
-// echo(phpinfo());
-echo("\nFolder upload_tmp_dir is: " . ini_get('upload_tmp_dir'));
-// echo("\nFolder sys_get_tmp_dir is: " . sys_get_tmp_dir());
-echo("\nHere.\n");
+
+// Check if a POST has been received.
 if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
     echo("REQUEST_METHOD === POST\n");
+
+   // Check the $_FILES array for the uploaded file. 
    if (isset($_FILES['file'])) {
-        echo("FILES is set.\n");
+       
         $errors = [];
         $path = 'uploads/';
-       // $extensions = ['mp3'];
-       $file_tmp = $_FILES['file']['tmp_name'];
-       $file_name = $_FILES['file']['name'];
-       $file = $path . $file_name;
-       move_uploaded_file($file_tmp, $file);
-       echo("File uploaded: ". $file);
-       var_dump($_FILES);
-        //$all_files = count($_FILES['files']['tmp_name']);
-    //    echo("Number of files: " . $all_files);
-        for ($i = 0; $i < $all_files; $i++) {
-            echo("Inside if statement.");
-            $file_name = $_FILES['file']['name'][$i];
-            $file_tmp = $_FILES['file']['tmp_name'][$i];
-        //    $file_type = $_FILES['file']['type'][$i];
-            $file_size = $_FILES['file']['size'][$i];
-            $file_ext = strtolower(end(explode('.', $_FILES['file']['name'][$i])));
+        $extensions = ['mp3'];
 
-            $file = $path . $file_name;
-            echo(print_r("File name: " . $file_name));
-            echo(print_r("File temporary name: " . $file_tmp));
-            echo(print_r("File type: " . $file_type));
-            echo(print_r("File size: " . $file_size));
-            echo(print_r("Complete file identifier: " . $file));
-            // if (!in_array($file_ext, $extensions)) {
-            //     $errors[] = 'Extension not allowed: ' . $file_name . ' ' . $file_type;
-            // }
+        // Get uploaded temporary location.
+        $file_tmp = $_FILES['file']['tmp_name'];
 
-            //if ($file_size > 2097152) {
-           //     $errors[] = 'File size exceeds limit: ' . $file_name . ' ' . $file_type;
-           // }
+        // Get file name.
+        $file_name = $_FILES['file']['name'];
 
-           // if (empty($errors)) {
-                move_uploaded_file($file_tmp, $file);
-                echo("File uploaded.");
-           // }
-        }
+        // Create complete file URI by concatenating the path and the file name.
+        $file = $path . $file_name;
 
-        //if ($errors) echo(print_r($errors));
-
-
+        // Move the uploaded fille to the correct location.
+        move_uploaded_file($file_tmp, $file);
+        
+        echo("File uploaded: ". $file);
+        var_dump($_FILES);
 
    }
 }
