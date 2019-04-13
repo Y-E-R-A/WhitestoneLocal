a599a7431c55db7c4940caa3c027379119ecd4f0
Gustavohernandez1 <gustavo.hernandez2@upr.edu>
2019-04-13 19:28:04 -0400

    Added two new functions and new conditions
    The two functions are in charge of handling the votes submitted by the user. The first function update the choice list related to an active voting question. The second function gets the choices when a voting question is closed. The new conditions are to store the choices of an active voting question in a list once its load and to empty the list when the voting question is closed

--- handler/VotingChoiceHandler.py
+++ handler/VotingChoiceHandler.py
@@ -2,11 +2,14 @@ from flask import jsonify, request
 from dao.VotingChoiceDAO import VotingChoiceDAO
 from dao.VotingQuestionDAO import VotingQuestionDAO
 
-
+###NEW##
+choiceResultArray = []
+activeVotingQuestion = False
+########
 class VotingChoiceHandler:
 
     def builtVotingChoiceDict(self, altID, vID, choice, votes):
-        # Built voting choice information dictionary
+        # Voting Choice Info dictionary
         result = {}
         result['altID'] = altID
         result['vID'] = vID
@@ -16,11 +19,11 @@ class VotingChoiceHandler:
 
 
     def mapVotingChoiceByVID(self, row):
-        # Voting Choice dictionary
+
         result = {}
         result['altID'] = row[0]
         result['choice'] = row[1]
-        result['votes'] = row[2]
+        result['votes']= row[2]
         return result
 
     def mapToUpdateVotingChoiceDict(self, vID, altID, votes):
@@ -44,13 +47,14 @@ class VotingChoiceHandler:
             for r in result:
                 mapped_result.append(self.mapVotingChoiceByVID(r))
 
-            return jsonify(Choice=mapped_result), 200
+            return jsonify(Choice=mapped_result)
 
 
 
     def getActiveVotingChoiceByvID(self, vID):
         # Handler for getting the active voting alternatives of a voting
-
+        global activeVotingQuestion
+        global choiceResultArray
         result = VotingChoiceDAO().getActiveVotingChoiceByVID(vID)
         mapped_result = []
 
@@ -60,13 +64,39 @@ class VotingChoiceHandler:
         else:
             for r in result:
                 mapped_result.append(self.mapVotingChoiceByVID(r))
-
+            ##This is new######################################
+            #Stores the choices when there is an active voting question. Store the choices
+            # once the first load is executed
+            if (activeVotingQuestion == False):
+                print(choiceResultArray)
+                choiceResultArray = mapped_result
+                activeVotingQuestion = True
+            ####################################################
             return jsonify(Choice=mapped_result), 200
 
+    ##NEW##########################################
+    ##function that updates the choices as the user submit his/her choice
+    def updateChoiceArray(self, json):
+        global choiceResultArray
+        choice = json.get('choice')
+        for x in range(len(choiceResultArray)):
+            if (choiceResultArray[x]['choice'] == choice):
+                choiceResultArray[x]['votes'] = choiceResultArray[x]['votes'] + 1
+                break
+        print(choiceResultArray)
+        return jsonify(choiceResultArray)
 
+    ##Functions that gets the current updated choices with  its result
+    def getChoiceResult(self):
+        global choiceResultArray
+        if (len(choiceResultArray) != 0):
+            return jsonify(Choice=choiceResultArray), 201
+        else:
+            return jsonify(Error="Malformed update request"), 400
+        ###########################################################
 
     def insertChoiceJSON(self, json):
-        # Insert new voting alternative
+    # Insert voting alternative
 
         vID = json.get('vID')
         choice = json.get('choice');
@@ -85,12 +115,18 @@ class VotingChoiceHandler:
 
             else:
 
-                return jsonify(Error="Unexpected attributes in post request"), 400
-
+                return jsonify(Error="Unexpected attributes in post request"), 404
 
 
     def updateVotingChoice(self, form):
-        # Handle the update request to set the final results of a voting
+        ###NEW###
+        global choiceResultArray
+        global activeVotingQuestion
+        if(activeVotingQuestion):
+            choiceResultArray.clear()
+            activeVotingQuestion = False
+        print("array of choices",choiceResultArray)
+        ########
         altID = form['altID']
         vID = form['vID']
         votes = form['votes']
@@ -105,6 +141,6 @@ class VotingChoiceHandler:
                 if votes and altID and vID:
                     VotingChoiceDAO().updateVotingChoice(votes, altID)
                     result = self.mapToUpdateVotingChoiceDict(vID, altID, votes)
-                    return jsonify(Choice=result), 200
+                    return jsonify(Choice=result), 201
                 else:
                     return jsonify(Error="Unexpected attributes in update request"), 400
