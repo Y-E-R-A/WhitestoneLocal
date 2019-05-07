46bb52e6d619d40dcc07f1614aeda91e4377ec82
Ariel Torres <ariel.torres1@upr.edu>
2019-05-06 23:59:18 -0400

    Updated the installer with more commands

--- server/installer.sh
+++ server/installer.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash				
 
 ##############################################################################################
@@ -8,8 +7,8 @@
 #          (server-side), and the whitestone database                                        #
 # Instructions: Open the terminal paste the following commands:                              #
 #       >> chmod +x <path of this script>                                                    #
-#          (Example:chmod +x /home/ariel/installer.sh)                                       #
-#	>> /home/ariel/installer.s                                                               #
+#          (Example:chmod +x /home/ariel/Whitestone/installer.sh)                            #
+#	>> /home/ariel/Whitestone/installer.sh                                                   #
 ##############################################################################################
 
 if [ "$(whoami)" != "root" ]			# Request to login as sudo
@@ -17,8 +16,6 @@ then
     sudo su -s "$0"
     exit
 fi
-
-
 
 echo "***WHITESTONE INSTALLATION***"
 echo "This script is used to install the Whitestone application (server-side) and the"
@@ -51,69 +48,62 @@ fi
 function installPackages(){
 	sudo yum install postgresql-server postgresql-contrib #Install PostgreSQL
 	sudo yum install git								  #Install Git
+    sudo yum update httpd                                 #Install Apache
+    sudo yum install httpd  
+    sudo yum install https://centos7.iuscommunity.org/ius-release.rpm  
+    sudo yum install python35u
+    sudo yum install python35u-pip
+    sudo yum install python35u-devel
+    sudo pip3.5 install flask
+    sudo pip3.5 install flask-cors
+    sudo pip3.5 install py-radius
 
-
+    #Enable services                           
+    sudo systemctl enable httpd
+    sudo systemctl enable postgresql
 }
 
 function installWhitestoneApplication(){
-	sudo git clone https://whitestone.uprm.edu/Y-E-R-A/Whitestone.git
+	sudo git clone https://whitestone.uprm.edu/Y-E-R-A/Whitestone.git #Downloads the Whitestone application
+    sudo cp Whitestone /var/www/html/Whitestone
+
+    #Give ownership permissions to Apache over the audio folder
+    sudo chown apache /var/www/html/Whitestone/static/audio
+    sudo chgrp apache /var/www/html/Whitestone/static/audio
+    sudo chmod 766 /var/www/html/Whitestone/static/audio
+}
+
+
+function installDatabase(){
+    get_DB_Backup_File(){
+						# Get the DB backup files path
+	read -p "Enter the Whitestone database backup file absolute path:" path
+	echo $path				# Return the Whitestone database backup file path
+}
+
+createDB(){
+
+	DB_Name="whitestone"			# The database name
+	DB_Admin="whitestoneadmin"		# The database administrator
+	sudo -u postgres dropdb --if-exists $DB_Name	# Drop the whitetsone db if exist
+	sudo -u postgres createdb $DB_Name		# Create a new db named whitestone
 
 }
 
 
-DATE=`date +%d%m%Y` 				# Backup date
-TIME=`date +"%d%m%Y_%H%M"`			# Backup date and time
-DIR=/var/www/Whitestone_Backups/${TIME}/	# Default destination of backup
-DB_Name="whitestone"				# The database name
-DB_Admin="whitestoneadmin"			# The database administrator
-FILENAME=${DB_Name}_${DATE}.sql			# The database backup name
+path=$(get_DB_Backup_File)
+while [ ! -f $path ]	 			# Loop until backup path exists
+do	
+	echo "ERROR: File $path not exists" 	# Otherwise the file does not exists
+	get_DB_Backup_File			# Call recursively the function until the input path is correct
+done 
 
 
-echo "The default directory to backup Whitestone Application is:"$DIR		 
-read -p "Do you want to change the directory? Enter yes or no:" input
-
-while [ $input ! = "yes" ] && [ $input ! = "no" ]	# Validate the input yes or no
-do
-	read -p "Do you want to change the directory? Enter yes or no:" input
-done
-
-if [ $input = "yes" ]			# The user wants to change backup directory
-	then read -p "Enter the new directory to backup Whitestone DB:" Dir
-	echo $DIR			# Display the new directory selected by user
-else 
-
-	echo "The backup will be performed in the default directory"
-fi
-	
- 
-mkdir -p $DIR				# Creating Backups Directory if not exist
-path=${DIR}${FILENAME}			# The database backup full path
-
-echo "Enter the $DB_Admin user password"
-pg_dump $DB_Name -U $DB_Admin > $path	# Postgresql Whitestone DB dump or backup instruction
-if  [ $? = "1" ]; then 			# Check for errors
-	echo "Backup error"		# If "1" and errors occurred
-else
-	echo "Backup Complete:" $path 	# If "0" the db backup was a success
-fi
+createDB 					# Call the function to drop and create a empty db to for the restore
+restoreDB $path					# Restore all the existing data from the .bak file
 
 
-#gzip $filename
-
-##############################################################################################
-#                                                                                            #
-#                                 Whitestone App Backup                                      #
-##############################################################################################
-
-APP_NAME=whitestone                        # The application directory name
-APP=${APP_NAME}_${DATE}.tar.gz             # The application backup name including the date.
-SRCDIR=/var/www/html                       # Source backup folder.
-tar -cvf $DIR/$APP -C $SRCDIR Whitestone   # Create a tar compressed file    
-if  [ $? = "1" ]; then 			   # If "1" and errors occurred
-	echo "Whitestone Backup Error"
-else					   # If "0" the db backup was a success
-	echo "Whitestone Backup Complete:" ${DIR}${APP} 
-fi
+}
 
 
 exit 0
