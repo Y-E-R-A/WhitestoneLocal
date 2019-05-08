b262191ae39c3cf5ee9d05b30a8fb54b8c9304ef
Ariel Torres <ariel.torres1@upr.edu>
2019-05-07 23:15:58 -0400

    Completed installation script

--- server/installer.sh
+++ server/installer.sh
@@ -2,7 +2,7 @@
 
 ##############################################################################################
 # Author: Ariel Torres Perez                                                                 #   
-# Updated: 5/6/2019                                                                          #
+# Updated: 5/7/2019                                                                          #
 # Purpose: This script is used to install the Whitestone application                         #
 #          (server-side), and the whitestone database                                        #
 # Instructions: Open the terminal paste the following commands:                              #
@@ -46,12 +46,10 @@ else
 	echo "No packages will be installed."
 fi
 
-
-
 installPackages(){
-	sudo yum install postgresql-server postgresql-contrib #Install PostgreSQL
-	sudo yum install git								  #Install Git
-    sudo yum update httpd                                 #Install Apache
+	sudo yum install postgresql-server postgresql-contrib
+	sudo yum install git								  
+    sudo yum update httpd                                 
     sudo yum install httpd  
     sudo yum install https://centos7.iuscommunity.org/ius-release.rpm  
     sudo yum install python35u
@@ -66,53 +64,97 @@ installPackages(){
     sudo systemctl enable postgresql
 }
 
+##############################################################################################
+#                                     Whitestone Installation                                #
+##############################################################################################
 
-installWhitestoneApplication(){
-	sudo git clone https://whitestone.uprm.edu/Y-E-R-A/Whitestone.git #Downloads the Whitestone application
-    sudo cp Whitestone /var/www/html/Whitestone
-
-    #Give ownership permissions to Apache over the audio folder
-    sudo chown apache /var/www/html/Whitestone/static/audio
-    sudo chgrp apache /var/www/html/Whitestone/static/audio
-    sudo chmod 766 /var/www/html/Whitestone/static/audio
+get_Backup_File(){
+						# Get the backup files path
+	read -p "Enter the Whitestone distribution file absolute path:" path
+	echo $path				# Resturn the Whitestone Project backup path value
 }
 
-getApacheConfiguration(){
-    $whitestoneApacheFilesPath = "/var/www/html/Whitestone/server"
-    sudo cp /var/www/html/Whitestone/server/httpd.conf /etc/httpd/conf/httpd.conf
-    sudo cp /var/www/html/Whitedtone/server/ssl.conf /etc/httpd/conf.d/ssl.conf
+untar_Backup_File(){
 
+	echo "Distribution file path: $1"		# Backup file was found
+	tar -xvf $1 -C /var/www/html/           # Extract the contents of path to the directory specified 
+	if  [ $? = "0" ]; then 
+		echo "Extract Complete"		# If "0" restore was a sucess
+	else
+		echo "Extract Error" 		# Otherwise an error occurred
+	fi
 }
 
-getdatabaseFile(){         # Get the database files path
+get_DB_Backup_File(){
+						# Get the DB backup files path
 	read -p "Enter the Whitestone database file absolute path:" path
-	echo $path				# Return the Whitestone database file path
-    }
+	echo $path				# Return the Whitestone database backup file path
+}
 
-createDatabase(){
+restoreDB(){
+
+	DB_Name="whitestone"			# The database name
+	DB_Admin="whitestoneadmin"		# The database administrator
+
+
+						# Preparing folder for the backup
+	if  [ -f /var/lib/postgresql/Backups ]; then 
+		mkdir /var/lib/postgresql/Backups 
+	fi
+
+	psql $DB_Name -U $DB_Admin -W< $1	# Perform the backup process
+
+	if  [ $? = "1" ]; then 
+		echo "Creation error."		# If "1" an error occurred
+	else
+		echo "Creation completed." 	# If "0" restores was a success
+	fi
+
+}
+
+createDB(){
 
 	DB_Name="whitestone"			# The database name
 	DB_Admin="whitestoneadmin"		# The database administrator
 	sudo -u postgres dropdb --if-exists $DB_Name	# Drop the whitetsone db if exist
 	sudo -u postgres createdb $DB_Name		# Create a new db named whitestone
 
-    }
+}
 
-#path=$(get_DB_Backup_File)
-#while [ ! -f $path ]	 			# Loop until backup path exists
-#do	
-#	echo "ERROR: File $path not exists" 	# Otherwise the file does not exists
-#	get_DB_Backup_File			# Call recursively the function until the input path is correct
-#done 
+configureWhitestoneApplication(){
+
+    #Give ownership permissions to Apache over the audio folder
+    sudo chown apache /var/www/html/Whitestone/static/audio
+    sudo chgrp apache /var/www/html/Whitestone/static/audio
+    sudo chmod 766 /var/www/html/Whitestone/static/audio
+    
+    #Configure Apache using the predefined files
+    sudo cp /var/www/html/Whitestone/server/httpd.conf /etc/httpd/conf/httpd.conf
+    sudo cp /var/www/html/Whitedtone/server/ssl.conf /etc/httpd/conf.d/ssl.conf
+}
 
 
-createDatabase 					# Call the function to drop and create a empty db to for the restore
-#restoreDB $path					# Restore all the existing data from the .bak file
+path=$(get_Backup_File)
+while [ ! -f $path ]	 			# Loop until backup path exists
+do	
+	echo "ERROR: File $path not exists" 	# Otherwise the file does not exists
+	get_Backup_File				# Call recursively the function until the input path is correct
+done 
+
+untar_Backup_File $path				# Call function to extract the tar backup file
+
+path=$(get_DB_Backup_File)
+while [ ! -f $path ]	 			# Loop until backup path exists
+do	
+	echo "ERROR: File $path not exists" 	# Otherwise the file does not exists
+	get_DB_Backup_File			# Call recursively the function until the input path is correct
+done 
 
 
-
-
+createDB 					# Call the function to drop and create a empty db to for the restore
+restoreDB $path					# Restore all the existing data from the .bak file
+configureWhitestoneApplication
 sudo systemctl start postgresql
 sudo systemctl start httpd
-
+sudo systemct daemon-reload
 exit 0
